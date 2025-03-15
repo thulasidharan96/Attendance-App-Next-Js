a07879f787a8c00ac3634d2efe50573c034d0d54
thulasidharan96 <thulasidharankvp@gmail.com>
2025-03-15 17:28:22 +0530

    PWA Optimization and Logo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,9 +16,9 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <link rel="manifest" href="/manifest.json" />
+        <link rel="manifest" href="manifest.json" />
         <meta name="theme-color" content="#ffffff" />
-        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+        <link rel="apple-touch-icon" href="./public/image" />
       </Head>
       <Component {...pageProps} />
     </>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,13 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Link from "next/link";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head></Head>
+      <Head>
+        <link rel="icon" href="/favicon.svg" sizes="any" />
+        <link rel="icon" type="image/svg" href="/image.svg" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
