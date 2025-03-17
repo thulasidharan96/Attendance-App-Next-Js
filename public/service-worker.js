f0ba7aea53e1ae22e447847fe832a5b70aeaf905
thulasidharan96 <thulasidharankvp@gmail.com>
2025-03-17 18:02:17 +0530

    Offline Response By Enhance SW fetch Activity

--- public/service-worker.js
+++ public/service-worker.js
@@ -9,10 +9,16 @@ self.addEventListener("install", (event) => {
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    fetch(event.request).catch(() =>
-      caches
-        .match(event.request)
-        .then((response) => response || caches.match("/offline.html"))
-    )
+    fetch(event.request).catch(async () => {
+      const cache = await caches.open("pwa-cache");
+
+      // Check if the request is for a page (navigate event)
+      if (event.request.mode === "navigate") {
+        return cache.match("/offline.html"); // Serve offline.html when offline
+      }
+
+      // Otherwise, serve cached assets if available
+      return cache.match(event.request);
+    })
   );
 });
