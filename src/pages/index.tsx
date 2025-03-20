1c0a772e3919e63c26b5e3f1c85edbbac604dbce
thulasidharan96 <thulasidharankvp@gmail.com>
2025-03-20 18:10:45 +0530

    Test #12

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -476,7 +476,7 @@ export default function Home() {
         </motion.div>
         <button
           className="absolute top-4 right-4 text-gray-500 hover:text-gray-700"
-          onClick={() => router.push("/onboarding")}
+          onClick={() => router.push("/onboard")}
         >
           OnBoarding
         </button>
